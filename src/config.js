7942b0322ee14df7b9943328280102915408df79
muhammad-salman-saleem <msalman1221998@gmail.com>
2024-07-04 18:23:51 +0500

    okta-react-sample

---
+++ src/config.js
@@ -0,0 +1,19 @@
+const CLIENT_ID = process.env.CLIENT_ID || '0oagv4c8vpsjR4Ubv5d7';
+const ISSUER = process.env.ISSUER || 'https://dev-99098661.okta.com/oauth2/default';
+const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
+const REDIRECT_URI = `${window.location.origin}/login/callback`;
+
+// eslint-disable-next-line
+export default {
+  oidc: {
+    clientId: CLIENT_ID,
+    issuer: ISSUER,
+    redirectUri: REDIRECT_URI,
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
+  },
+  resourceServer: {
+    messagesUrl: 'http://localhost:8000/api/messages',
+  },
+};
